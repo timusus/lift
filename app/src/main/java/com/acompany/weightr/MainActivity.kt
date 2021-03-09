b6aa80e8ca26af7dfde38a23a8c3b44e9a078f1e
Tim Malseed <t.malseed@gmail.com>
2021-03-09 13:47:56 +1100

    Implemented basic navigation

--- app/src/main/java/com/acompany/weightr/MainActivity.kt
+++ app/src/main/java/com/acompany/weightr/MainActivity.kt
@@ -10,7 +10,10 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
+import androidx.navigation.compose.*
 import com.acompany.data.AppRepository
+import com.acompany.weightr.features.components.exercise.LazyExerciseList
 import com.acompany.weightr.features.components.session.LazySessionList
 import com.acompany.weightr.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,26 +26,51 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var repository: AppRepository
 
+    sealed class Screen(val route: String, val name: String) {
+        object Sessions : Screen("sessions", "Sessions")
+        object Exercises : Screen("sessions/{sessionId}/exercises", "Exercises")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             val sessions by repository.getSessions().collectAsState(emptyList())
+            val navController = rememberNavController()
+            val screens = listOf(Screen.Sessions, Screen.Exercises)
+
             AppTheme {
                 Scaffold(
                     topBar = {
-                        TopAppBar(title = { Text(text = getString(R.string.app_name)) })
+                        val navBackStackEntry by navController.currentBackStackEntryAsState()
+                        val currentRoute = navBackStackEntry?.arguments?.getString(KEY_ROUTE)
+                        TopAppBar(title = { Text(text = screens.firstOrNull { screen -> screen.route == currentRoute }?.name ?: "weightr") })
                     },
                     content = { paddingValues ->
-                        LazySessionList(
-                            sessions = sessions,
-                            modifier = Modifier.padding(paddingValues)
-                        ) { session ->
-                            Timber.d("$session")
+                        NavHost(navController, startDestination = Screen.Sessions.route) {
+                            composable(Screen.Sessions.route) {
+                                LazySessionList(
+                                    sessions = sessions,
+                                    modifier = Modifier.padding(paddingValues)
+                                ) { session ->
+                                    navController.navigate("sessions/${session.id}/exercises")
+                                }
+                            }
+                            composable(
+                                route = Screen.Exercises.route,
+                                arguments = listOf(navArgument("sessionId") { type = NavType.IntType })
+                            ) { backStackEntry ->
+                                LazyExerciseList(
+                                    exercises = sessions.filter { session -> session.id == backStackEntry.arguments!!.getInt("sessionId") }.flatMap { it.exercises },
+                                    modifier = Modifier.padding(paddingValues)
+                                ) { session ->
+                                    Timber.d("$session")
+                                }
+                            }
                         }
-                    }
+                    },
                 )
             }
         }
     }
-
 }
