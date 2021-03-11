5fe28c8792fdba67cd8bfa386b73d3649ebefd3e
Andrew Neal <andrewdneal@gmail.com>
2021-03-10 21:12:43 -0600

    update exerciseweightextfield

--- app/src/main/java/com/acompany/weightr/features/exercises/components/ExerciseScreen.kt
+++ app/src/main/java/com/acompany/weightr/features/exercises/components/ExerciseScreen.kt
@@ -1,15 +1,18 @@
 package com.acompany.weightr.features.exercises.components
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.acompany.data.AppRepository
 import com.acompany.data.model.RoutineExercise
 import com.acompany.weightr.features.exercises.components.ExerciseHelper.initialWeight
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
 @Composable
@@ -17,11 +20,22 @@ fun ExerciseScreen(
     routineId: Long,
     repository: AppRepository
 ) {
+    val scope = rememberCoroutineScope()
     val routineExercises by repository.getRoutineExercises(listOf(routineId)).collectAsState(emptyList())
     var editingRoutineExercise by remember { mutableStateOf(null as RoutineExercise?) }
     ExerciseModelSheet(
         sheetContent = {
-            ExerciseWeightTextField(editingRoutineExercise?.initialWeight())
+            editingRoutineExercise?.let { routine ->
+                ExerciseWeightTextField(
+                    exerciseName = routine.exercise.name,
+                    initialWeight = editingRoutineExercise?.initialWeight(),
+                    onWeightChanged = { weight ->
+                        scope.launch {
+                            repository.updateExercise(routine.exercise.id, weight)
+                        }
+                    }
+                )
+            }
         },
         content = {
             StartSessionButton {
@@ -39,11 +53,21 @@ fun ExerciseScreen(
 }
 
 @Composable
-private fun ExerciseWeightTextField(initialWeight: Float?) {
+private fun ExerciseWeightTextField(
+    exerciseName: String,
+    initialWeight: Float?,
+    onWeightChanged: (Float?) -> Unit
+) {
     val textState = remember { mutableStateOf(initialWeight?.toString() ?: "") }
+    Text(text = "Edit $exerciseName")
     OutlinedTextField(
         value = textState.value,
-        onValueChange = { textState.value = it }
+        label = { Text(text = "One Rep Max") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        onValueChange = { textFieldValue ->
+            textState.value = textFieldValue
+            onWeightChanged(textFieldValue.toFloatOrNull())
+        }
     )
 }
 
