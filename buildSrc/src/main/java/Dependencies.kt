721f4107e0a68648afb9f473434811fa2bbe8bc4
Andrew Neal <andrewdneal@gmail.com>
2021-03-16 02:08:56 -0500

    update android gradle plugin

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,5 +1,5 @@
 object Dependencies {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha09"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha10"
 
     object Accompanist {
         private const val version = "0.6.2"
