3712a9dd3e173ffa4ef95d14f4f5d0546995505c
Andrew Neal <andrewdneal@gmail.com>
2021-03-17 16:00:28 -0500

    move AFAB into common

--- app/src/main/java/com/acompany/lift/common/components/AnimatedFloatingActionButton.kt
+++ app/src/main/java/com/acompany/lift/common/components/AnimatedFloatingActionButton.kt
@@ -1,4 +1,4 @@
-package com.acompany.lift.features.exercises.components
+package com.acompany.lift.common.components
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
