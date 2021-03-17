87f9e4a4c0cab8f0442df2bc000ff6957c724dcf
Andrew Neal <andrewdneal@gmail.com>
2021-03-17 17:55:10 -0500

    hoist navController

--- app/src/main/java/com/acompany/lift/features/main/components/LIftScaffold.kt
+++ app/src/main/java/com/acompany/lift/features/main/components/LIftScaffold.kt
@@ -5,14 +5,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.navigate
-import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun LiftScaffold(
+    navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
-    val navController = rememberNavController()
     Scaffold(
         modifier = modifier,
         content = { paddingValues ->
--- app/src/main/java/com/acompany/lift/features/main/components/LiftApp.kt
+++ app/src/main/java/com/acompany/lift/features/main/components/LiftApp.kt
@@ -1,11 +1,13 @@
 package com.acompany.lift.features.main.components
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
 import com.acompany.lift.theme.AppTheme
 
 @Composable
 fun LiftApp() {
+    val navController = rememberNavController()
     AppTheme {
-        LiftScaffold()
+        LiftScaffold(navController)
     }
 }
