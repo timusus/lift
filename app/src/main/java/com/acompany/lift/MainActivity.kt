ffb3c5dfa9cba88ef93aaca6b59c6e07d81060c3
Tim Malseed <t.malseed@gmail.com>
2021-03-18 16:04:38 +1100

    Keep screen on

--- app/src/main/java/com/acompany/lift/MainActivity.kt
+++ app/src/main/java/com/acompany/lift/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.acompany.lift
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.acompany.lift.features.main.components.LiftApp
@@ -11,6 +12,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         setContent {
             LiftApp()
