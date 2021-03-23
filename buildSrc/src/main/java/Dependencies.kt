193ae9143f146d31d32211d4bf4a7932f990dcee
Andrew Neal <andrewdneal@gmail.com>
2021-03-23 10:11:57 -0500

    bump gradle plugin

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,5 +1,5 @@
 object Dependencies {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha10"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha11"
 
     object Accompanist {
         private const val version = "0.6.2"
