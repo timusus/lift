f2a19c44f839564b0f511933e8cc3b442240f832
Tim Malseed <tim@codeacious.com>
2021-04-07 21:09:13 +1000

    Updated gradle plugin

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,5 +1,5 @@
 object Dependencies {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha12"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-alpha13"
 
     object Accompanist {
         private const val version = "0.6.2"
